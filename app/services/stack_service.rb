4e295e46d7bd6078aef16d83514a02dc3de0ad1e
KhaledBadawyM <khaledbadawi881@gmail.com>
2022-10-28 20:12:03 +0200

    never search for max unless stack has changed

--- app/services/stack_service.rb
+++ app/services/stack_service.rb
@@ -1,22 +1,26 @@
 class Stack
     def initialize
         @first = nil
+        @has_change = true
+        @current_max = nil
     end
 
     def push(value)
-        raise "Value must be a numeric type!" if !is_numeric(value)
+        raise "Value must be a integer type!" if !is_integer(value)
         @first = Node.new(value, @first)
+        @has_change = true
     end
         
     def pop
         raise "Stack is empty" if is_empty?
         value = @first.value
         @first = @first.next_node
+        @has_change = true
         value
     end
 
-    def is_numeric(value)
-        value.is_a?(Numeric)
+    def is_integer(value)
+        value.is_a?(Integer)
     end
 
     def is_empty?
@@ -24,6 +28,14 @@ class Stack
     end
 
     def max
+        if @has_change
+            @current_max = get_max
+            @has_change = false
+        end
+        return @current_max
+    end
+
+    def get_max
         max_num = @first.value
         current_node = @first.next_node
         while current_node != nil
@@ -34,3 +46,4 @@ class Stack
         end
         max_num
     end
+end
